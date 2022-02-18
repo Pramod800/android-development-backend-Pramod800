514ccaa7e5d39b80e9590d1e2d01f0e801e50c79
Pramod800 <timilsinapramod8888@gmail.com>
2022-02-18 12:23:24 +0545

    add blog

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,4 +5,36 @@ const router = new express.Router();
 const multer = require("multer");
 const upload = require("../uploads/upload");
 
+router.post(
+  "/addblog",
+  upload.single("thumbnail"),
+  auth.verifyUser,
+  async function (req, res) {
+    const title = req.body.title;
+    const subtitle = req.body.subtitle;
+    const description = req.body.description;
+    const thumbnail = req.file.filename;
+    const postData = new Post({
+      title: title,
+      description: description,
+      subtitle: subtitle,
+      thumbnail: thumbnail,
+      postedBy: req.userInfo,
+    });
+    req.userInfo.password = undefined;
+    await postData
+      .save()
+      .then(() => {
+        res.status(200).json({
+          postData,
+          message: "post has been sucessfully created",
+          success: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
+
 module.exports = router;
