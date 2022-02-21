c954aa3f4459a80a127e437d75c9933a3f9d2c8e
Pramod800 <timilsinapramod8888@gmail.com>
2022-02-21 12:26:27 +0545

    Comment added

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -75,4 +75,18 @@ router.get("/blog", async (req, res) => {
     });
 });
 
+router.post("/comment", auth.verifyUser, (req, res) => {
+  const comment = { Text: req.body.commentText, postedBy: req.userInfo._id };
+  Post.findByIdAndUpdate(req.body.blogId, {
+    $push: { Comments: comment },
+  })
+    .populate("Comments.postedBy", "username")
+    .then((docs) => {
+      res.json({ success: true, commentcount: docs.Comments.length });
+    })
+    .catch((e) => {
+      res.json({ message: e, success: false });
+    });
+});
+
 module.exports = router;
